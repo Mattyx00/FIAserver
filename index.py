8ec58e645ae4db8e17d25aa49446839bfa47917b
Mattyx00 <matteosacco00@gmail.com>
2022-01-25 14:53:59 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, Response
+from controller import *
+from machineLearning import executeMachineLearning
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/FIA/consiglio', methods=['POST'])
+def contribuisci():
+    try:
+        content = request.get_json(silent=True)
+        if not 'risposte' in content:
+            return Response("campi invalidi", status=300)
+        response = analyze_data(content['risposte'])
+        response = {"dipartimento": str(response)}
+        response = json.dumps(response)
+        return Response(response, status=200, mimetype='application/json')
+    except Exception as e:
+        print(e)
+        return Response("Internal error", status=500)
+
+
+@app.route('/FIA/machineLearning', methods=['GET'])
+def machineLearning():
+    try:
+        response = executeMachineLearning()
+        response = {"score": str(response)}
+        response = json.dumps(response)
+        return Response(response, status=200, mimetype='application/json')
+    except Exception as e:
+        print(e)
+        return Response("Internal error", status=500)
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
